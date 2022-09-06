e0c057a7a02586da117dba5d4f25cab93ec4b717
lidya <lidya. abraham@student.moringaschool.com>
2022-09-06 18:58:44 +0300

    commpleted assignment

--- main.js
+++ main.js
@@ -3,10 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className = 'hidden'
 
+const likeButtons = document.querySelectorAll('.like-glyph')
+function myFun() {
+    likeButtons.forEach(function(btn) {
+        btn.addEventListener('click', function(event) {
 
+            if (event.target.innerHTML == EMPTY_HEART) {
+                mimicServerCall("")
+                    .then(function(resp) {
+                        event.target.innerHTML = FULL_HEART
+                        event.target.classList.add('activated-heart')
 
+                    })
+                .catch(function(error) {
+                    let modal = document.querySelector('#modal')
+                    modal.classList.remove('hidden')
+                    modal.innerText = "Random server error. Try again."
 
+                    setTimeout(() => {
+                        modal.classList.add('hidden')
+                    }, 3000);
+                })
+            } else {
+                event.target.innerHTML = EMPTY_HEART
+                event.target.classList.remove('activated-heart')
+            }
+        })
+    })
+}
+myFun()
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
